866e7efeb7444d396fe32fef8ff8cc6122f1fb57
GeorgiosKalantzis <georgekalantzis997@gmail.com>
2019-02-12 01:51:43 +0200

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,199 @@
+/* Ονοματεπώνυμο : Γεώργιος Καλαντζής
+ * ΑΕΜ: 8818
+ * Τηλέφωνο: 6946275377
+ * email: gkalantz@ece.auth.gr */
+package snakeGame_PartA;
+
+import java.util.*;
+import java.lang.Math;
+
+public class Game {
+	int round;
+
+	public Game() {
+		round = 0;
+	}
+
+	public Game(int r) {
+		round = r;
+	}
+
+	public int getRound() {
+		return round;
+	}
+
+	public void setRound(int r) {
+		this.round = r;
+	}
+
+	/*
+	 * The method creates a Map , in which the keys are the random dices and the
+	 * values are the player's ID , so it returns a sorted by key Map , with the
+	 * corresponding ID's as values , !! CAUTION !! If the dices are same , then the
+	 * Map will contain only one element, but we resolve that case later in the main
+	 * method
+	 */
+
+	public Map<Integer, Integer> setTurns(ArrayList<Player> players) {
+		// The return map
+		Map<Integer, Integer> PlayersMap = new HashMap<Integer, Integer>();
+		int dice;
+		// To get access to the Player objects inside the ArrayList
+		Player player;
+
+		for (int i = 0; i < players.size(); i++) {
+			dice = 1 + (int) (Math.random() * 6);
+			player = players.get(i);
+			PlayersMap.put(dice, player.getPlayerId());
+		}
+
+		return PlayersMap;
+	}
+	/*
+	 * I create a new game , the board and then the players , which I add them in a
+	 * ArrayList , to pass it as an argument to setTurns method , in order to
+	 * determine the turns, later inside a for loop , the players play alternately ,
+	 * once they are two , and finally the winner is printed , along with statistics
+	 * and other info
+	 */
+
+	public static void main(String[] args) {
+
+		int N = 20;
+		int M = 10;
+
+		Game game = new Game(0);
+
+		Board board = new Board(N, M, 3, 3, 6);
+
+		board.createBorad();
+		board.createElementBoard();
+		// The players
+		Player player1 = new Player(0, "player1", 0, board);
+		HeuristicPlayer player2 = new HeuristicPlayer(1, "player2", 0, board);
+
+		ArrayList<Player> players = new ArrayList<Player>();
+
+		players.add(player1);
+		players.add(player2);
+		// Call setTurns , to determine the turn
+		Map<Integer, Integer> PlayersTurn = game.setTurns(players);
+
+		// Cause we have only two players we can alternately select who is going to play
+		// , by just multiplying by -1 the variable k with initial value 1
+		int k = 1;
+
+		// Set the first player
+		for (Integer key : PlayersTurn.keySet()) {
+			// In case the players had the same dice , by default I choose that the first
+			// player will be the one that plays with random moves
+			if (PlayersTurn.get(key) == 0 || PlayersTurn.size() == 1) {
+				k = k * 1;
+				break;
+			}
+
+			if (PlayersTurn.get(key) == 1) {
+
+				k = k * (-1);
+				break;
+
+			}
+
+		}
+
+		int[] currentPosition = new int[players.size()];
+		int newPosition = 0;
+		String winnerName = null;
+		String theFaster = null;
+		int dice;
+		// We use this variable to calculate later the exact number of the rounds
+		double roundCounter = 1.0;
+
+		for (int i = 0; i < players.size(); i++) {
+			currentPosition[i] = 0;
+		}
+
+		System.out.println("*********** The game begins **********");
+		System.out.println();
+
+		for (;;) {
+			roundCounter++;
+
+			if (k == 1) {
+
+				dice = 1 + (int) (Math.random() * 6);
+
+				newPosition = player1.move(currentPosition[player1.getPlayerId()], dice)[0];
+				currentPosition[player1.getPlayerId()] = newPosition;
+
+				if (newPosition >= N * M) {
+
+					// Player1 terminated first
+					theFaster = player1.getName();
+					// Does he have also the greatest score ?
+					if (player1.getScore() > player2.getScore()) {
+
+						winnerName = player1.getName();
+
+					}
+
+					break;
+				}
+
+			}
+
+			if (k == -1) {
+
+				newPosition = player2.getNextMove(currentPosition[player2.getPlayerId()]);
+				currentPosition[player2.getPlayerId()] = newPosition;
+				player2.statistics();
+
+				if (newPosition >= N * M) {
+
+					// Player2 terminated first
+					theFaster = player2.getName();
+					// Does he have also the greatest score ?
+					if (player2.getScore() > player1.getScore()) {
+
+						winnerName = player2.getName();
+
+					}
+
+					break;
+				}
+
+			}
+
+			if (newPosition >= N * M) {
+				break;
+			}
+
+			// Multiply by -1 , so the players play alternately
+			k = k * (-1);
+
+		}
+
+		// If the first one that finished didn't also had bigger total score , then the
+		// winner is the faster
+		if (winnerName == null) {
+			winnerName = theFaster;
+		}
+
+		// Cause the counter inside the for loop , counts twice every round
+		// we use a upper division to calculate the integer number of rounds
+		game.round = (int) Math.ceil(roundCounter / 2.0);
+
+		System.out.println();
+		System.out.println("*********** The game is over *********");
+		System.out.println();
+		System.out.println("Rounds played: " + game.round);
+
+		for (int i = 0; i < players.size(); i++) {
+			System.out.println(((Player) players.get(i)).getName() + " gathered " + ((Player) players.get(i)).getScore()
+					+ " points");
+		}
+		System.out.println("***********  " + winnerName + " won the game!!!" + "  ************");
+
+	}
+
+}
---
+++ HeuristicPlayer.java
@@ -0,0 +1,133 @@
+package snakeGame_PartA;
+
+import java.util.ArrayList;
+
+public class HeuristicPlayer extends Player {
+
+	ArrayList<Integer[]> path;
+
+	// Constructor that calls the Player's class constructor
+	public HeuristicPlayer(int id, String n, int s, Board b) {
+		super(id, n, s, b);
+		path = new ArrayList<Integer[]>();
+
+	}
+
+	// Copy constructor
+	public HeuristicPlayer(HeuristicPlayer obj) {
+		playerId = obj.playerId;
+		name = obj.name;
+		score = obj.score;
+		// Use the copy constructor of board to create a new one independent from the
+		// original
+		board = new Board(obj.board);
+
+	}
+
+	/*
+	 * I create a new HeuristicPlayer object using the copy constructor and inside
+	 * the copy constructor , I call Board's copy constructor to create a new copy
+	 * board. Finally the method implements the evaluation function that I defined
+	 * and returns it.
+	 */
+	public double evaluate(int currentPos, int dice) {
+
+		HeuristicPlayer objClone = new HeuristicPlayer(this);
+
+		int[] moveTable = new int[5];
+
+		// Make the move
+		moveTable = objClone.move(currentPos, dice);
+
+		// Evaluation function
+		double evalFunc = 0.7 * (moveTable[0] - currentPos) + 0.3 * (objClone.score);
+
+		return evalFunc;
+	}
+
+	public int getNextMove(int currentPos) {
+
+		// Array for possibleMoves
+		double[] possibleMoves = new double[6];
+
+		int[] moveTable = new int[5];
+
+		// To save the final dice
+		int bestMove = 0;
+		// For comparisons , in order to find the best dice
+		double maxScore = 0;
+
+		for (int i = 1; i <= possibleMoves.length; i++) {
+
+			possibleMoves[i - 1] = this.evaluate(currentPos, i);
+
+			if (possibleMoves[i - 1] >= maxScore) {
+
+				maxScore = possibleMoves[i - 1];
+
+				bestMove = i;
+
+			}
+
+		}
+
+		// Actual move with our original object's board
+		moveTable = this.move(currentPos, bestMove);
+
+		int points = this.getScore();
+
+		// The info for the ArrayList
+		Integer[] info = { bestMove, moveTable[0] - currentPos, points, moveTable[1], moveTable[2], moveTable[3],
+				moveTable[4] };
+
+		this.path.add(info);
+
+		return moveTable[0];
+	}
+	/*
+	 * I save the ArrayList's size to a variable round, which is the current round
+	 * then I am printing some info about the heuristic player and finally , I
+	 * iterate over my ArrayList to gather all the statistics for the player and
+	 * print them
+	 */
+
+	public void statistics() {
+		
+		//Round's number
+		int Round = this.path.size();
+
+		System.out.println("The player chose dice " + this.path.get(Round - 1)[0] + " at round " + Round
+				+ " was bitten by " + this.path.get(Round - 1)[3] + " snakes" + " and climbed "
+				+ this.path.get(Round - 1)[4] + " ladders ");
+		
+			
+		// To collect all the information we want till the current round
+		int snakeVisits = 0;
+		int ladderVisits = 0;
+		int redAppleVisits = 0;
+		int blackAppleVisits = 0;
+
+		for (int i = 0; i < Round; i++) {
+
+			snakeVisits = snakeVisits + this.path.get(i)[3];
+
+			ladderVisits = ladderVisits + this.path.get(i)[4];
+
+			redAppleVisits = redAppleVisits + this.path.get(i)[5];
+
+			blackAppleVisits = blackAppleVisits + this.path.get(i)[6];
+
+		}
+		
+
+		System.out.println("Player , so far , visited " + snakeVisits + " snakes");
+
+		System.out.println("Player, so far , visited " + ladderVisits + " ladders");
+
+		System.out.println("Player, so far , ate " + redAppleVisits + " red apples");
+
+		System.out.println("Player, so far , ate " + blackAppleVisits + " black apples");
+
+	}
+
+}
